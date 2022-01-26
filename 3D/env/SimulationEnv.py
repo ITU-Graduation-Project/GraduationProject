a605771fac21ed149657443f4114ce7cad2c16ed
MuhammetAkcann <akcan18@itu.edu.tr>
2022-01-26 23:50:45 +0300

    deleted files added

---
+++ 3D/env/SimulationEnv.py
@@ -0,0 +1,172 @@
+import gym
+from gym import spaces
+import numpy as np
+from render.SimulationGraph import SimulationGraph
+from env.uav import UAV
+
+LOOKBACK_WINDOW_SIZE = 40
+
+
+class SimulationEnv(gym.Env):
+    """A 3D flight simulation environment for OpenAI gym"""
+    metadata = {'render.modes': ['live', 'file', 'none']}
+    visualization = None
+
+    def __init__(self):
+        super(SimulationEnv, self).__init__()
+        self.uav1 = UAV()
+        self.uav2 = UAV()
+        self.uav_list = [self.uav1, self.uav2]
+        self.current_reward = [0, 0]  # first is orientation reward, second is distance reward
+        self.dt = 0.5  # each step time
+        self.g = 9.81
+        # Set actionSpace
+        # first variable is roll change: 0 -> left; 1 -> steady; 2 -> right
+        # second variable is pitch change: 0 -> down; 1 -> steady; 2 -> up
+        # first variable is speed change: 0 -> decelerate; 1 -> steady; 2 -> accelerate
+        self.action_space = spaces.Tuple((
+            spaces.Discrete(3),
+            spaces.Discrete(3),
+            spaces.Discrete(3),
+            spaces.Discrete(3),
+            spaces.Discrete(3),
+            spaces.Discrete(3)))
+
+        # Set Observation space
+        self.observation_space = spaces.Box(low=0, high=1, shape=(11, 1), dtype=np.float16)
+
+    def get_na(self):
+        der_pu = np.array([self.uav1.der_x, self.uav1.der_y, self.uav1.der_z])
+        der_pt = np.array([self.uav2.der_x, self.uav2.der_y, self.uav2.der_z])
+        pu = np.array(self.uav1.position)
+        pt = np.array(self.uav2.position)
+
+        temp1 = (der_pu @ (pt - pu)) / np.linalg.norm(pt - pu)
+        temp2 = (der_pt @ (pu - pt)) / np.linalg.norm(pu - pt)
+
+        na = temp1 - temp2
+
+        return na
+
+    def get_nd(self, beta1=8, beta2=0.7, d_max=30, d_min=0):
+
+        distance = np.linalg.norm(np.array(self.uav_list[0].position) - np.array(self.uav_list[1].position))
+
+        f_t = (d_max - abs(distance)) / (d_max - d_min)
+        s_t = 1 - np.exp(-(abs(distance) - d_min) ** beta2)
+        return_val = beta1 * f_t * s_t
+        return return_val
+
+    """def ds(self):
+        distance = np.linalg.norm(np.array(self.uav_list[0].position) - np.array(self.uav_list[1].position))
+        if distance < 30:
+            return_value = distance / 2
+        elif 30 < distance < 60:
+            return_value = 30 - distance / 2
+        else:
+            return_value = - np.sqrt(distance - 30) * 4
+        return return_value"""
+
+    def calculate_advantage(self, w1=0.5, w2=0.5):
+        self.current_reward[0], self.current_reward[1] = self.get_na(), self.get_nd()
+        return w1 * self.get_na() + w2 * self.get_nd()
+
+    def _next_observation(self):
+        obs = np.zeros(11)
+
+        # first uav
+        obs[0] = 1 / (1 + np.exp(-(self.uav1.position[0] - self.uav2.position[0])/30))
+        obs[1] = 1 / (1 + np.exp(-(self.uav1.position[1] - self.uav2.position[1])/30))
+        obs[2] = 1 / (1 + np.exp(-(self.uav1.position[2] - self.uav2.position[2])/30))
+
+        obs[3] = 1 / (1 + np.exp(-self.uav1.roll/45))
+        obs[4] = 1 / (1 + np.exp(-self.uav1.pitch/45))
+        obs[5] = 1 / (1 + np.exp(-self.uav1.yaw/90))
+
+        obs[6] = 1 / (1 + np.exp(-self.uav1.speed/5))
+
+        # second uav
+        obs[7] = 1 / (1 + np.exp(-self.uav2.roll/45))
+        obs[8] = 1 / (1 + np.exp(-self.uav2.pitch/45))
+        obs[9] = 1 / (1 + np.exp(-self.uav2.yaw/90))
+
+        obs[10] = 1 / (1 + np.exp(-self.uav2.speed/5))
+        return obs
+
+    def _take_action(self, action):
+        for i, uav in enumerate(self.uav_list):
+            uav.roll = uav.roll + (action[i * 3 + 0] - 1) * self.dt * uav.der_roll
+            uav.pitch = uav.pitch + (action[i * 3 + 1] - 1) * self.dt * uav.der_pitch
+            uav.speed = uav.speed + (action[i * 3 + 2] - 1) * self.dt * uav.der_speed
+
+            # check for boundaries
+            if uav.roll > uav.max_roll:
+                uav.roll = uav.max_roll
+            elif uav.roll < -uav.max_roll:
+                uav.roll = -uav.max_roll
+
+            if uav.pitch > uav.max_pitch:
+                uav.pitch = uav.max_pitch
+            elif uav.pitch < -uav.max_pitch:
+                uav.pitch = -uav.max_pitch
+
+            if uav.speed > uav.max_speed:
+                uav.speed = uav.max_speed
+            elif uav.speed < uav.min_speed:
+                uav.speed = uav.min_speed
+
+            n = 1 / np.cos(np.radians(uav.roll))
+            der_yaw = np.degrees((self.g * np.sqrt(n ** 2 - 1)) / uav.speed)
+            uav.yaw += der_yaw * self.dt
+            uav.yaw = uav.yaw % 360
+
+            uav.der_x = uav.speed * np.cos(np.radians(uav.yaw)) * np.cos(np.radians(uav.pitch))
+            uav.der_y = uav.speed * np.sin(np.radians(uav.yaw)) * np.cos(np.radians(uav.pitch))
+            uav.der_z = uav.speed * np.sin(np.radians(uav.pitch))
+
+            uav.position[0] += uav.der_x * self.dt
+            uav.position[1] += uav.der_y * self.dt
+            uav.position[2] += uav.der_z * self.dt
+        # print("uav.roll, uav.pitch, uav.speed:", uav.roll, uav.pitch, uav.speed)
+        # print("der_x, der_y, der_z:", der_x, der_y, der_z)
+        # print("uav.position:", uav.position)
+
+    def step(self, action, episode=None):
+        # Execute one time step within the environment
+        self._take_action(action)
+
+        # print("self.calculate_advantage():", self.calculate_advantage())
+
+        self.current_step += 1
+
+        thresh = 12
+
+        reward = self.calculate_advantage()
+
+        done = 1 if reward > thresh else 0
+        obs = self._next_observation()
+
+        return obs, reward, done, {}
+
+    def reset(self):
+        # Reset the state of the environment to an initial state
+        self.uav1 = UAV()
+        self.uav2 = UAV()
+        self.uav_list = [self.uav1, self.uav2]
+        self.current_step = 0
+
+        return self._next_observation()
+
+    def render(self, mode='live', **kwargs):
+        # Render the environment to the screen
+        if mode == 'live':
+            if self.visualization is None:
+                self.visualization = SimulationGraph(
+                    kwargs.get('title', None))
+            if self.current_step is not None:
+                self.visualization.render(self.uav1.position, self.uav2.position, self.current_reward)
+
+    def close(self, episode):
+        if self.visualization != None:
+            self.visualization.close(episode)
+            self.visualization = None
---
+++ 3D/env/dqn.py
@@ -0,0 +1,162 @@
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
+from keras.callbacks import TensorBoard
+from keras.optimizers import adam_v2 as Adam
+import os
+from collections import deque
+import numpy as np
+import time
+import random
+
+DISCOUNT = 0.99
+REPLAY_MEMORY_SIZE = 100_000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 10_000  # Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 512  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 50  # Terminal states (end of episodes)
+MODEL_NAME = '2x256'
+
+
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+        self._log_write_dir = self.log_dir
+
+    def set_model(self, model):
+        self.model = model
+
+        self._train_dir = os.path.join(self._log_write_dir, 'train')
+        self._train_step = self.model._train_counter
+
+        self._val_dir = os.path.join(self._log_write_dir, 'validation')
+        self._val_step = self.model._test_counter
+
+        self._should_write_train_graph = False
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    def on_train_end(self, _):
+        pass
+
+    def update_stats(self, **stats):
+        with self.writer.as_default():
+            for key, value in stats.items():
+                tf.summary.scalar(key, value, step=self.step)
+                self.writer.flush()
+
+
+class DQNAgent:
+    def __init__(self):
+        # Main model
+        self.model = self.create_model()
+
+        # Target network
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        # An array with last n steps for training
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        # Custom tensorboard object
+        self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
+
+        # Used to count when to update target network with main network's weights
+        self.target_update_counter = 0
+
+    # Adds step's data to a memory replay array
+    # (observation space, action, reward, new observation space, done)
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    # Queries main network for Q values given current observation space (environment state)
+    def get_qs(self, state):
+        # print("state.shape:", state.shape)
+        q_vals = self.model.predict(state.reshape(1, 11))[0]
+        # print("q vals:", q_vals)
+        return q_vals
+
+    def create_model(self, path=None):
+        """
+            Builds a deep neural net which predicts the Q values for all possible
+            actions given a state. The input should have the shape of the state
+            (which is 4 in CartPole), and the output should have the same shape as
+            the action space (which is 2 in CartPole) since we want 1 Q value per
+            possible action.
+            :return: the Q network
+            """
+        if path is not None:
+            q_net = tf.keras.models.load_model(path)
+            return q_net
+        q_net = Sequential()
+        initializer = tf.keras.initializers.VarianceScaling(
+            scale=0.1, mode='fan_in', distribution='uniform')
+        q_net.add(Dense(512, input_dim=11, activation='tanh', kernel_initializer=initializer, bias_initializer='zeros'))
+        q_net.add(Dense(1024, activation='tanh', kernel_initializer=initializer, bias_initializer='zeros'))
+        q_net.add(Dense(512, activation='tanh', kernel_initializer=initializer, bias_initializer='zeros'))
+        q_net.add(Dense(128, activation='tanh', kernel_initializer='zeros'))
+        q_net.add(Dense(27, kernel_initializer='he_uniform'))
+        q_net.compile(optimizer=tf.optimizers.Adam(learning_rate=0.001), loss='mse')
+
+        return q_net
+
+    # Trains main network every step during episode
+    def train(self, terminal_state):
+        # Start training only if certain number of samples is already saved
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            print("not training")
+            return
+        #print("training")
+        # Get a minibatch of random samples from memory replay table
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        # Get current states from minibatch, then query NN model for Q values
+        current_states = np.array([transition[0] for transition in minibatch])
+        current_qs_list = self.model.predict(current_states)
+
+        # Get future states from minibatch, then query NN model for Q values
+        # When using target network, query it, otherwise main network should be queried
+        new_current_states = np.array([transition[3] for transition in minibatch])
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        X = []
+        y = []
+
+        # Now we need to enumerate our batches
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            # print("current state:", current_state)
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            # Update Q value for given state
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            # And append to our training data
+            X.append(current_state)
+            y.append(current_qs)
+
+        # Fit on all samples as one batch, log only on terminal state
+        self.model.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False,
+                       callbacks=[self.tensorboard])
+
+        # Update target network counter every episode
+        self.target_update_counter += 1
+
+        # If counter reaches set value, update target network with weights of main network
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
